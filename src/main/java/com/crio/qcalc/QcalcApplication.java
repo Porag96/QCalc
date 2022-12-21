a2f7e00695b3f51979ac2663595524ffded8002a
Porag96 <poragjyotilakhimpur@gmail.com>
2022-12-22 01:12:54 +0530

    First commit

---
+++ src/main/java/com/crio/qcalc/QcalcApplication.java
@@ -0,0 +1,16 @@
+package com.crio.qcalc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QcalcApplication {
+
+	public static void main(String[] args) {
+		// SpringApplication.run(QcalcApplication.class, args);
+		LogicCalculator calc = new LogicCalculator();
+		calc.OR(8, 6);
+		calc.printResult();
+	}
+
+}
